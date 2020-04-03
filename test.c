24bfd7f1605f660866b4d58bb6378eeac6780c11
achrefbs <siachrefboularess@gmail.com>
2020-04-03 16:21:38 +0000

    added header to test

--- test.c
+++ test.c
@@ -1,12 +1,14 @@
 #include "simple_shell.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 int main()
 {
 	char *test;
 	int n = 256, c;
 	test = malloc(sizeof(char) * n);
-	c = _getline(&test,fileno(stdout));
+	c = _getline(&test,STDIN_FILENO);
 	printf("string is \"%s\"\nchars read : %d\n", test, c);
   	free(test);
 	return 0;
