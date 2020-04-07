04c2bc2a41f5f6d0b78862a337ace56ee186012d
Yosri-ctrl <yosribouabid@gmail.com>
2020-04-07 20:56:47 +0100

    declaration

--- hsh.c
+++ hsh.c
@@ -5,12 +5,8 @@
 **/
 int main(void)
 {
-    int i = 0;
-    char *sep = " \t\r\n\a", *token = NULL, **str2 = NULL;
-    char *buffer;
-    size_t bufsize = 20;
+    char **str2 = NULL;
 
-    buffer = malloc(sizeof(char) * 20);
     str2 = malloc(sizeof(char *) * 100);
     if (str2 == NULL)
     return (0);
@@ -19,15 +15,8 @@ int main(void)
     while(1)
     {
         putchar('$');
-        getline(&buffer, &bufsize, stdin);
-        token = strtok(buffer, sep);
-        while(token != NULL)
-        {
-        str2[i] = token;
-        i++;
-        token = strtok(NULL, sep);
-        }
-        str2[i] = NULL;
+
+        str2 = handle_input();
 
         execcmd(str2[0], str2);
     }
--- simple_shell.h
+++ simple_shell.h
@@ -20,6 +20,7 @@ char *_strcpy(char *dest, char *src);
 char *_strtok(char *str, const char delim);
 char *_substring(char *string, int position, int length);
 void execcmd(char *cmd, char **array);
+char **handle_input();
 
 /*structs*/
 
