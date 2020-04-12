4e542331d83d933c997dfefd1afac4e34829af80
Yosri-ctrl <yosribouabid@gmail.com>
2020-04-12 15:30:08 +0100

    exit

--- shell.c
+++ shell.c
@@ -1,20 +1,30 @@
 #include "shell.h"
-int main(void) {
-  char *line,**str;
-  int i;
-  while(1){
-  _putchar('$');
-  line = _getline();
-  str = split(line);
-  free(line);
-  _exec(str[0],str);
+/**
+*main - tha main function
+*Return: Always 0
+*/
+int main(void) 
+{
+	char *line, **str;
+	int i;
+	while(1)
+	{
+		_putchar('$');
+		_putchar(' ');
 
-  /**free**/
-  for (i = 0;str[i] != NULL;i++)
-  {
-    free(str[i]);
-  }
-    free(str);
-  }
-  return(0);
+		line = _getline();
+		str = split(line);
+		free(line);
+		if ((strcmp(str[0], "exit") == 0))
+			break;
+		_exec(str[0],str);
+
+		/**free**/
+		for (i = 0;str[i] != NULL;i++)
+		{
+			free(str[i]);
+		}
+		free(str);
+	}
+	return(0);
 }
