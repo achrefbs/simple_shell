f75a1afcf0586f46b41c751fa437bbcd7dece470
achrefbs <siachrefboularess@gmail.com>
2020-04-15 12:33:11 +0000

    update

---
+++ exec.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+*_exec - execute the command
+*@cmd: the command
+*@array: array holdin the argments
+*Return: Return 0 in success
+*/
+int _exec(char *cmd, char **array)
+{
+	pid_t pid;
+	int status;
+
+	if (cmd == NULL || array == NULL)
+		return (-1);
+
+	pid = fork();
+	if (pid < 0)
+	{
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(cmd, array, NULL) != 0)
+		return (-1);
+		if (execve(cmd, array, environ) != 0)
+		{
+			perror("fail to execute");
+			return (-1);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+	}
+	return (1);
+}
