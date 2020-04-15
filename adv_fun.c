56df01ea879b7e04e470a25d2b9639beb47879f5
achrefbs <siachrefboularess@gmail.com>
2020-04-15 21:13:29 +0000

    update atoi

--- adv_fun.c
+++ adv_fun.c
@@ -107,19 +107,26 @@ s[j] = h;
  */
 int _atoi(char *s)
 {
-int i, sign = 1;
-unsigned int n = 0;
-for (i = 0; s[i] != '\0'; i++)
+int i, sign = 1, num = 0, res;
+
+
+for (i = 0; s[i] != '\0';)
 {
 if (s[i] == '-')
-sign *= -1;
-if (s[i] >= '0' && s[i] <= '9')
 {
-if ((s[i - 1] >= '0' && s[i - 1] <= '9') || n == 0)
-n = (n * 10) + (s[i] - '0');
+sign = sign * (-1);
+i++;
+}
+
+else if ((s[i] >= '0') && (s[i] <= '9'))
+{
+num = num * 10 + (s[i] - '0');
+i++;
+}
 else
-break;
+i++;
 }
-}
-return (n *sign);
+res = (sign *num);
+
+return (res);
 }
